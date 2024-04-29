8bd8dc8b08ce596785880e3ae5744531ea1488e8
jdrolex <168436097+jdrolex@users.noreply.github.com>
2024-04-29 23:18:55 +0530

    Update ex5.java

--- ex5.java
+++ ex5.java
@@ -8,24 +8,28 @@
 #include<sys/wait.h>
 int main()
 {
-int fd[2],n;
-char buffer[100];
-pid_t p;
-pipe(fd); //creates a unidirectional pipe with two end fd[0] and fd[1]
-p=fork();
-if(p>0) //parent
-{
-printf("Parent Passing value to child\n");
-write(fd[1],"hello\n",6); //fd[1] is the write end of the pipe
-wait();
+    int fd[2], n;
+    char buffer[100];
+    pid_t p;
+    pipe(fd); //creates a unidirectional pipe with two ends fd[0] and fd[1]
+    p=fork();
+    if(p>0) //parent
+    {
+        printf("Parent Passing value to child\n");
+        write(fd[1],"hello\n",6); //fd[1] is the write end of the pipe
+        close(fd[1]); // Close the write end of the pipe
+        wait(NULL); // Wait for the child process to finish
+    }
+    else // child
+    {
+        close(fd[1]); // Close the write end of the pipe
+        printf("Child printing received value\n");
+        n=read(fd[0],buffer,100); //fd[0] is the read end of the pipe
+        write(1,buffer,n);
+    }
+    return 0;
 }
-else // child
-{
-printf("Child printing received value\n");
-n=read(fd[0],buffer,100); //fd[0] is the read end of the pipe
-write(1,buffer,n);
-}
-}
+
 
 
 //MESSAGE QUEUE FOR WRITER PROCESS 
