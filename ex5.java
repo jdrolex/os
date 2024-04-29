1284d173474e839fc9c9a675b6a207ff928ae754
jdrolex <168436097+jdrolex@users.noreply.github.com>
2024-04-29 23:21:11 +0530

    Update ex5.java

--- ex5.java
+++ ex5.java
@@ -116,28 +116,50 @@ int main()
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 using namespace std;
-  
+
 int main()
 {
     // ftok to generate unique key
-    key_t key = ftok("shmfile",65);
-  
+    key_t key = ftok("shmfile", 65);
+    if (key == -1) {
+        perror("ftok");
+        return 1;
+    }
+
     // shmget returns an identifier in shmid
-    int shmid = shmget(key,1024,0666|IPC_CREAT);
-  
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    if (shmid == -1) {
+        perror("shmget");
+        return 1;
+    }
+
     // shmat to attach to shared memory
-    char *str = (char*) shmat(shmid,(void*)0,0);
-  
-    gets(str);
-  
-    printf("Data written in memory: %s\n",str);
-      
-    //detach from shared memory 
-    shmdt(str);
-  
+    char *str = (char*) shmat(shmid, nullptr, 0);
+    if (str == (char*)(-1)) {
+        perror("shmat");
+        return 1;
+    }
+
+    // Read input from user
+    cout << "Enter a string: ";
+    fgets(str, 1024, stdin);
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Data written in memory: %s\n", str);
+
+    // Detach from shared memory
+    if (shmdt(str) == -1) {
+        perror("shmdt");
+        return 1;
+    }
+
     return 0;
 }
+
 
 SHARED MEMORY FOR READER PROCESS
 #include <iostream>
