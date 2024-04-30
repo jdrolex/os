f804ce9d6a3eeb6d26baf552072efe4ebbbd85b3
jdrolex <168436097+jdrolex@users.noreply.github.com>
2024-04-30 11:34:23 +0530

    Update ex7.java

--- ex7.java
+++ ex7.java
@@ -110,6 +110,7 @@ public class ContiguousMemoryManager {
 
 
 /**************************************************************************************************************** */
+//best fit
 #include<stdio.h>
 #define max 25
 int main()
@@ -154,5 +155,45 @@ lowest=10000;
 printf("\nProcess_no \tProcess_size \tBlock_no \tBlock_size \tFragment");
 for(i=1;i<=np && ff[i]!=0;i++)
 printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,p[i],ff[i],b[ff[i]],frag[i]);
+return 0;
+}
+************************
+    //first fit
+    int main()
+{
+int bno,pno,bs[20],ps[20],i,j;
+printf("Enter no of Blocks.\n");
+scanf("%d",&bno);
+for(i=0;i<bno;i++)
+{
+printf("Enter the %dst Block size:",i);
+scanf("%d",&bs[i]);
+}
+printf("Enter no of Process.\n");
+scanf("%d",&pno);
+for(i=0;i<pno;i++)
+{
+printf("Enter the size of %dst Process:",i);
+scanf("%d",&ps[i]);
+}
+for(i=0;i<bno;i++)
+{
+for(j=0;j<pno;j++)
+{
+if(ps[j]<=bs[i])
+{
+printf("The Process %d allocated to %d\n",j,bs[i]);
+ps[j]=10000;
+break;
+}
+}
+}
+for(j=0;j<pno;j++)
+{
+if(ps[j]!=10000)
+{
+printf("The Process %d is not allocated\n",j);
+}
+}
 return 0;
 }
